731e28e1d075de16eda4853b845a4dffa0742d1a
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-06-28 08:44:00 +0200

    [PAGOPA-1834] fix: removed fields previously left for backward compatibility

--- src/main/java/it/gov/pagopa/wispconverter/technicalsupport/service/model/EventCategoryEnum.java
+++ src/main/java/it/gov/pagopa/wispconverter/technicalsupport/service/model/EventCategoryEnum.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.wispconverter.technicalsupport.service.model;
 
 public enum EventCategoryEnum {
-    INTERFACCIA, INTERNO, // TODO to be removed after WISP SOAP Converter update
     INTERFACE,
     INTERNAL
 }
--- src/main/java/it/gov/pagopa/wispconverter/technicalsupport/service/model/OutcomeEnum.java
+++ src/main/java/it/gov/pagopa/wispconverter/technicalsupport/service/model/OutcomeEnum.java
@@ -1,8 +1,6 @@
 package it.gov.pagopa.wispconverter.technicalsupport.service.model;
 
 public enum OutcomeEnum {
-
-    INVIATA, INVIATA_KO, RICEVUTA, RICEVUTA_KO, NO_RICEVUTA, CAMBIO_STATO, // TODO to be removed after WISP SOAP Converter update
     SEND,
     SEND_FAILURE,
     RECEIVED,
