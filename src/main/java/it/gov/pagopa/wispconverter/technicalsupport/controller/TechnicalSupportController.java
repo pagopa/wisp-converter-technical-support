1cf9b4aa626a7c05ad9694a261ea3dbad9433a17
Andrea De Rinaldis <117269497+andrea-deri@users.noreply.github.com>
2024-08-07 16:54:07 +0200

    feat: add search-by-sessionId new API (#11)

--- src/main/java/it/gov/pagopa/wispconverter/technicalsupport/controller/TechnicalSupportController.java
+++ src/main/java/it/gov/pagopa/wispconverter/technicalsupport/controller/TechnicalSupportController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -35,13 +34,13 @@ public class TechnicalSupportController {
     private final ReService reService;
     private final TechnicalSupportMapperDto technicalSupportMapperDto;
 
-    @ApiResponses( value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Successfully retrieved event",
-            content = @Content( schema = @Schema(implementation = ReEventResponse.class)))
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved event",
+                    content = @Content(schema = @Schema(implementation = ReEventResponse.class)))
     })
-    @GetMapping(value = "/organizations/{"+ORGANIZATION+"}/notice-number/{"+NOTICE_NUMBER+"}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/organizations/{" + ORGANIZATION + "}/notice-number/{" + NOTICE_NUMBER + "}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ReEventResponse findByNoticeNumber(
             @PathVariable(name = ORGANIZATION) @Schema(example = "12345678900", description = "Id dell'EC") String organizationId,
             @PathVariable(name = NOTICE_NUMBER) @Schema(example = "3982273480765907", description = "Numero avviso") String noticeNumber,
@@ -56,13 +55,13 @@ public class TechnicalSupportController {
                 .build();
     }
 
-    @ApiResponses( value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Successfully retrieved event",
-            content = @Content( schema = @Schema(implementation = ReEventResponse.class)))
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved event",
+                    content = @Content(schema = @Schema(implementation = ReEventResponse.class)))
     })
-    @GetMapping(value = "/organizations/{"+ORGANIZATION+"}/iuv/{"+IUV+"}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/organizations/{" + ORGANIZATION + "}/iuv/{" + IUV + "}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ReEventResponse findByIuv(
             @PathVariable(name = ORGANIZATION) @Schema(example = "12345678900", description = "Id dell'EC") String organizationId,
             @PathVariable(name = IUV) @Schema(example = "982273480765907", description = "Identificativo univoco del versamento") String iuv,
@@ -76,13 +75,32 @@ public class TechnicalSupportController {
                 .build();
     }
 
-    @ApiResponses( value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Successfully retrieved event",
-            content = @Content( schema = @Schema(implementation = ReEventResponse.class)))
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved event",
+                    content = @Content(schema = @Schema(implementation = ReEventResponse.class)))
     })
-    @GetMapping(value = "/operation-id/{"+OPERATION_ID+"}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/session-id/{" + SESSION_ID + "}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ReEventResponse findBySessionId(
+            @PathVariable(name = SESSION_ID) @Schema(example = "12345678900_aa0518a9-194d-4de2-999f-e462c3746e24", description = "Session identifier of the flow") String sessionId,
+            @RequestParam(name = DATE_FROM) @Schema(example = "2024-03-18", description = "Data di ricerca DA") LocalDate dateFrom,
+            @RequestParam(name = DATE_TO) @Schema(example = "2024-03-18", description = "Data di ricerca A") LocalDate dateTo) {
+        List<ReEventDto> bySessionId = reService.findBySessionId(dateFrom, dateTo, sessionId);
+        List<ReEvent> reEventList = technicalSupportMapperDto.toReEventList(bySessionId);
+        return ReEventResponse.builder()
+                .count(reEventList.size())
+                .data(reEventList)
+                .build();
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved event",
+                    content = @Content(schema = @Schema(implementation = ReEventResponse.class)))
+    })
+    @GetMapping(value = "/operation-id/{" + OPERATION_ID + "}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ReEventResponse findByOperationId(
             @PathVariable(name = OPERATION_ID) @Schema(example = "fcfcbe1e-987a-4914-8fd2-d1e7b67762bf", description = "Identificativo dell'operazione eseguita") String operationId,
             @RequestParam(name = DATE_FROM) @Schema(example = "2024-03-18", description = "Data di ricerca DA") LocalDate dateFrom,
